747ed721cb21937586f72b32ecd74b8a025f5e62
jomapormentilla <jomapormentilla@gmail.com>
2020-12-16 14:43:49 -0500

    commit tree c15005d5026dfceaa86b303b93bdc2ceac0726bc

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,19 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate_student
+    student = Student.find(params[:id])
+
+    if student.active == false
+      student.update(active: true)
+    else
+      student.update(active: false)
+    end
+    
+    redirect_to student_path(student.id)
   end
 
   private
